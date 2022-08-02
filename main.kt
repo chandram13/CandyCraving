2676d3d393fb804966ab7e23cf383d4c77974c35
Marvish <mschandr@uci.edu>
2022-08-02 13:31:07 -0700

    Updated functions.

--- main.kt
+++ main.kt
@@ -5,11 +5,52 @@ class Choosing a Candy Based on Craving {
 class Chocolates()
 
 fun askChocolate() {
-    majorBrands = ["3 Musketeers","Almond Joy","Baby Ruth","Butterfinger","Cadbury","Heath","Kinder","Lindor",
+    majorChocolates = ["3 Musketeers","Almond Joy","Baby Ruth","Butterfinger","Cadbury","Heath","Kinder","Lindor",
     "M&M's","Milky Way","Reese's","Snicker's","Toblerone","Twix"]
-    for (m in majorBrands) {
+    for (m in majorChocolates) {
         println(m)
     }
     val chosenChocolate = readLine()
     println("I have decided to pick" + $chosenChocolate + ".")
+}
+
+fun askSour(){
+    majorSours = ["Nerds","Trolli","Sour Patch","Airheads","Toxic Waste","Jolly Rancher's","Starburst","SweetTarts"]
+    for (m in majorSours) {
+        println(m)
+    }
+    val chosenSour = readLine()
+    println("I have decided to pick" + $chosenSour + ".")
+}
+fun askGummy(){
+    majorGummies = ["Nerds","Swedish Fish","Trolli","Haribo","Life Savers","Great Value","Albanese","Starburst","Jolly Rachers","Black Forest","Airheads", "Skittles"]
+    for (m in majorGummies) {
+        println(m)
+    }
+    val chosenGummy = readLine()
+    println("I have decided to pick" + $chosenGummy + ".")
+}
+fun askCaramel(){
+    majorCaramels = ["Werther's Original","Kraft America","Cadbury Caramello","Dove", "Milky Way","Riesen","Rollo","100 Grand", "Zero", "Ghiradelli", "Payday","Lindt", "Milk Duds"]
+    for (m in majorCaramels) {
+        println(m)
+    }
+    val chosenCaramel = readLine()
+    println("I have decided to pick" + $chosenCaramel + ".")
+ }
+fun askLollipop(){
+    majorLollipops = ["Dum Dums","Tootsie Pops","Ring Pop","Jumbo Push Pop","Charms Blow Pop","Zolli Ball Popz"]
+    for (m in majorLollipops) {
+        println(m)
+    }
+    val chosenLollipop = readLine()
+    println("I have decided to pick" + $chosenLollipop + ".")
+}
+fun askHardCandy(){
+    majorHardCandies = ["Great Value Butterscotch","Life Savers","Jolly Rancher's","Great Value Strawberry Filled", "Werther's Original"]
+    for (m in majorHardCandies) {
+        println(m)
+    }
+    value chosenHardCandy = readLine()
+    println("I have decided to pick" + $chosenHardCandy + ".")
 }
